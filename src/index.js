6a6d0e58181a665c39a08362ed3302bf6e51e948
xwinkX <opalyuk.e88@ukr.net>
2022-06-19 23:04:50 +0300

    wew

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-// const { default: axios } = require('axios');
 const form = document.querySelector('#search-form');
 const input = document.querySelector('input');
 const gallery = document.querySelector('.gallery');
